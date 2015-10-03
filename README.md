0ddb9033a929efb0c02dbfb5ffbb224ae468abb3
Logan Spears <loganjspears@gmail.com>
2015-10-03 13:24:29 -0400

    added more tests for FEN

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 # chess
-
-### TODO
-* PGN Import / Export
-* Docs
-* Possible error w/ En_Passant and avoiding check / checkmate (board verification of valid moves vs. game validation)
--- fen_test.go
+++ fen_test.go
@@ -1,49 +1,37 @@
 package chess_test
 
 import (
-	"reflect"
 	"testing"
 
 	. "github.com/loganjspears/chess"
 )
 
 var (
-	validFENs = map[string]*GameState{
-		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1": &GameState{
-			Board: map[*Square]*Piece{
-				A1: WhiteRook, B1: WhiteKnight, C1: WhiteBishop, D1: WhiteQueen, E1: WhiteKing, F1: WhiteBishop, G1: WhiteKnight, H1: WhiteRook, // white back row
-				A2: WhitePawn, B2: WhitePawn, C2: WhitePawn, D2: WhitePawn, E2: WhitePawn, F2: WhitePawn, G2: WhitePawn, H2: WhitePawn, // white front row
-				A7: BlackPawn, B7: BlackPawn, C7: BlackPawn, D7: BlackPawn, E7: BlackPawn, F7: BlackPawn, G7: BlackPawn, H7: BlackPawn, // black front row
-				A8: BlackRook, B8: BlackKnight, C8: BlackBishop, D8: BlackQueen, E8: BlackKing, F8: BlackBishop, G8: BlackKnight, H8: BlackRook, // black back row
-			},
-			Turn:            White,
-			CastleRights:    &CastleRights{true, true, true, true},
-			EnPassantSquare: nil,
-			HalfMoveClock:   0,
-			MoveCount:       1,
-		},
+	testFENs = []string{
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+		"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1",
+		"rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2",
+		"rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
+		"7k/8/8/8/8/8/8/R6K w - - 0 1",
+		"7k/8/8/8/8/8/8/2B1KB2 w - - 0 1",
+		"8/8/8/4k3/8/8/8/R3K2R w KQ - 0 1",
+		"8/8/8/8/4k3/8/3KP3/8 w - - 0 1",
+		"8/8/5k2/8/5K2/8/4P3/8 w - - 0 1",
+		"r4rk1/1b2bppp/ppq1p3/2pp3n/5P2/1P1BP3/PBPPQ1PP/R4RK1 w - - 0 1",
+		"3r1rk1/p3qppp/2bb4/2p5/3p4/1P2P3/PBQN1PPP/2R2RK1 w - - 0 1",
+		"4r1k1/1b3p1p/ppq3p1/2p5/8/1P3R1Q/PBP3PP/7K w - - 0 1",
+		"5k2/ppp5/4P3/3R3p/6P1/1K2Nr2/PP3P2/8 b - - 1 32",
 	}
 )
 
-func TestValidFENS(t *testing.T) {
-	for fen, expectedState := range validFENs {
+func TestFEN(t *testing.T) {
+	for _, fen := range testFENs {
 		state, err := FEN(fen)
 		if err != nil {
 			t.Fatal("recieved unexpected error", err)
 		}
-		if !reflect.DeepEqual(expectedState, state) {
-			t.Fatalf("fen expected game state %+v but got %+v", expectedState, state)
+		if fen != state.String() {
+			t.Fatalf("fen expected board string %s but got %s", fen, state.String())
 		}
-	}
-}
-
-func TestFEN(t *testing.T) {
-	fen := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-	state, err := FEN(fen)
-	if err != nil {
-		t.Fatal("recieved unexpected error", err)
-	}
-	if fen != state.String() {
-		t.Fatalf("fen expected board string %s but got %s", fen, state.String())
 	}
 }
