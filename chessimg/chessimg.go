0f4c701512d6910ce05290fcf54b6d5b8cffd24e
Logan Spears <loganjspears@gmail.com>
2015-11-01 13:11:28 -0500

    switched api to encoder format

--- chessimg/chessimg.go
+++ chessimg/chessimg.go
@@ -1,55 +1,127 @@
-// Package chessimg provides ches image utilities
+// Package chessimg provides chess image utilities
 package chessimg
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"image"
 	"image/color"
+	"image/gif"
+	"image/png"
 	"io"
+	"log"
+	"os"
+	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/ajstarks/svgo"
 	"github.com/loganjspears/chess"
 	"github.com/lucasb-eyer/go-colorful"
 )
 
-// Config is used to configure options for WriteBoardSVG
-type Config struct {
+// SquareColors is designed to be used as an optional argument
+// to the WriteBoardSVG function.  It changes the default light and
+// dark square colors to the ones given.
+func SquareColors(light, dark color.Color) func(*Encoder) {
+	return func(e *Encoder) {
+		e.light = light
+		e.dark = dark
+	}
+}
+
+type Encoder struct {
+	w     io.Writer
 	light color.Color
 	dark  color.Color
 }
 
-func defaultConfig() *Config {
-	return &Config{
+func New(w io.Writer, options ...func(*Encoder)) *Encoder {
+	e := &Encoder{
+		w:     w,
 		light: color.RGBA{235, 209, 166, 1},
 		dark:  color.RGBA{165, 117, 81, 1},
 	}
-}
-
-// SquareColors is designed to be used as an optional argument
-// to the WriteBoardSVG function.  It changes the default light and
-// dark square colors to the ones given.
-func SquareColors(light, dark color.Color) func(*Config) {
-	return func(c *Config) {
-		c.light = light
-		c.dark = dark
-	}
-}
-
-// WriteBoardSVG outputs an SVG of the board to the writer.
-func WriteBoardSVG(w io.Writer, board chess.Board, options ...func(*Config)) {
-	config := defaultConfig()
 	for _, op := range options {
-		op(config)
+		op(e)
 	}
+	return e
+}
+
+func (e *Encoder) EncodeGIF(game *chess.Game, sideLength int, delay time.Duration) error {
+	anim := gif.GIF{LoopCount: 1}
+	palette := []color.Color{color.White, color.Black, e.light, e.dark}
+	for _, state := range game.States() {
+		b := &bytes.Buffer{}
+		if err := e.copy(b).EncodePNG(state.Board(), sideLength); err != nil {
+			return fmt.Errorf("chessimg: failed to encode state %s as png intermediate with error - %s", state, err.Error())
+		}
+		log.Println(b.String())
+		pngImg, err := png.Decode(b)
+		if err != nil {
+			return fmt.Errorf("chessimg: failed to decode png intermediate for state %s with error - %s", state, err.Error())
+		}
+		img := image.NewPaletted(pngImg.Bounds(), palette)
+		anim.Delay = append(anim.Delay, int(delay.Seconds()*100.0))
+		anim.Image = append(anim.Image, img)
+	}
+	return gif.EncodeAll(e.w, &anim)
+}
+
+func (e *Encoder) EncodePNG(board chess.Board, sideLength int) error {
+	// check width and height params
+	if sideLength <= 0 {
+		return errors.New("chessimg: width and height must be greater than zero")
+	}
+	// create temp svg file
+	svgFile, err := os.Create("temp.svg")
+	if err != nil {
+		return fmt.Errorf("chessimg: failed to create svg temp file with error - %s", err.Error())
+	}
+	defer svgFile.Close()
+	// write svg to temp
+	svgEn := e.copy(svgFile)
+	if err := svgEn.EncodeSVG(board); err != nil {
+		return err
+	}
+	// rsvg-convert -w 400 board.svg -o board.png
+	// http://manpages.ubuntu.com/manpages/precise/man1/rsvg-convert.1.html
+	l := fmt.Sprint(sideLength)
+	if err := exec.Command("rsvg-convert", "-w", l, "-h", l, "temp.svg", "-o", "board.png").Run(); err != nil {
+		return fmt.Errorf("chessimg: rsvg-convert command failed with error - %s", err.Error())
+	}
+	// write png to temp
+	pngFile, err := os.Create("temp.png")
+	if err != nil {
+		return fmt.Errorf("chessimg: failed to create png temp file with error - %s", err.Error())
+	}
+	defer pngFile.Close()
+	// copy png temp to writer
+	_, err = io.Copy(e.w, pngFile)
+	if err != nil {
+		return fmt.Errorf("chessimg: failed to copy png temp file to writer with error - %s", err.Error())
+	}
+	// delete temp files
+	if err := os.Remove("temp.svg"); err != nil {
+		return fmt.Errorf("chessimg: failed to delete svg temp file with error - %s", err.Error())
+	}
+	if err := os.Remove("temp.png"); err != nil {
+		return fmt.Errorf("chessimg: failed to delete png temp file with error - %s", err.Error())
+	}
+	return nil
+}
+
+func (e *Encoder) EncodeSVG(board chess.Board) error {
 	sqSize := 45
-	canvas := svg.New(w)
+	canvas := svg.New(e.w)
 	width := 8 * sqSize
 	height := 8 * sqSize
 	ranks := []string{"8", "7", "6", "5", "4", "3", "2", "1"}
 	files := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
 
-	lightHex := colorToHex(config.light)
-	darkHex := colorToHex(config.dark)
+	lightHex := colorToHex(e.light)
+	darkHex := colorToHex(e.dark)
 
 	canvas.Start(width, height)
 	canvas.Rect(0, 0, width, height)
@@ -69,7 +141,9 @@ func WriteBoardSVG(w io.Writer, board chess.Board, options ...func(*Config)) {
 				hasRank := int(sq.Rank()) == 9-rank
 				if hasFile && hasRank && p != nil {
 					xml := pieceXML(x1, y1, p)
-					io.WriteString(canvas.Writer, xml)
+					if _, err := io.WriteString(canvas.Writer, xml); err != nil {
+						return err
+					}
 				}
 			}
 			if file == 1 {
@@ -83,6 +157,15 @@ func WriteBoardSVG(w io.Writer, board chess.Board, options ...func(*Config)) {
 		}
 	}
 	canvas.End()
+	return nil
+}
+
+func (e *Encoder) copy(w io.Writer) *Encoder {
+	return &Encoder{
+		w:     w,
+		light: e.light,
+		dark:  e.dark,
+	}
 }
 
 func colorToHex(c color.Color) string {
--- chessimg/chessimg_test.go
+++ chessimg/chessimg_test.go
@@ -1,15 +1,51 @@
 package chessimg
 
 import (
+	"bytes"
 	"os"
 	"testing"
 
 	"github.com/loganjspears/chess"
 )
 
-func TestImg(t *testing.T) {
+func TestSVG(t *testing.T) {
 	game := chess.NewGame()
 	f, _ := os.Create("board.svg")
 	defer f.Close()
-	WriteBoardSVG(f, game.State().Board())
+
+	if err := New(f).EncodeSVG(game.State().Board()); err != nil {
+		t.Fatal(err)
+	}
 }
+
+func TestPNG(t *testing.T) {
+	game := chess.NewGame()
+	f, _ := os.Create("board.png")
+	defer f.Close()
+
+	if err := New(f).EncodePNG(game.State().Board(), 500); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPNGBytes(t *testing.T) {
+	game := chess.NewGame()
+	b := bytes.NewBuffer([]byte{})
+	if err := New(b).EncodePNG(game.State().Board(), 500); err != nil {
+		t.Fatal(err)
+	}
+	if len(b.Bytes()) == 0 {
+		t.Fatal("No bytes")
+	}
+}
+
+//
+// func TestGIF(t *testing.T) {
+// 	game := chess.NewGame()
+// 	f, _ := os.Create("game.gif")
+// 	defer f.Close()
+//
+// 	if err := New(f).EncodeGIF(game, 500, time.Second); err != nil {
+// 		t.Fatal(err)
+// 	}
+// }
