8311b31f42fd1f7ebec20190443c18822ce59072
Javier Escalada <kerrigan29a@gmail.com>
2018-11-16 16:16:55 +0100

    Pertf tests

--- move_test.go
+++ move_test.go
@@ -204,6 +204,73 @@ func TestPositionUpdates(t *testing.T) {
 	}
 }
 
+type perfTest struct {
+	pos           *Position
+	nodesPerDepth []int
+}
+
+/* https://www.chessprogramming.org/Perft_Results */
+var perfResults = []perfTest{
+	{pos: unsafeFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"), nodesPerDepth: []int{
+		20, 400, 8902,
+		// 197281, 4865609, 119060324, 3195901860, 84998978956, 2439530234167, 69352859712417
+	}},
+	{pos: unsafeFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"), nodesPerDepth: []int{
+		48, 2039,
+		// 97862, 4085603, 193690690
+	}},
+	{pos: unsafeFEN("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1"), nodesPerDepth: []int{
+		14, 191, 2812,
+		// 43238, 674624, 11030083, 178633661
+	}},
+	{pos: unsafeFEN("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"), nodesPerDepth: []int{
+		6, 264, 9467,
+		// 422333, 15833292, 706045033
+	}},
+	{pos: unsafeFEN("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"), nodesPerDepth: []int{
+		6, 264, 9467,
+		// 422333, 15833292, 706045033
+	}},
+	{pos: unsafeFEN("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8"), nodesPerDepth: []int{
+		44, 1486,
+		// 62379, 2103487, 89941194
+	}},
+	{pos: unsafeFEN("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10"), nodesPerDepth: []int{
+		46, 2079,
+		// 89890, 3894594, 164075551, 6923051137, 287188994746, 11923589843526, 490154852788714
+	}},
+}
+
+func TestPerfResults(t *testing.T) {
+	for _, perf := range perfResults {
+		countMoves(t, perf.pos, []*Position{perf.pos}, perf.nodesPerDepth, len(perf.nodesPerDepth))
+	}
+}
+
+func countMoves(t *testing.T, originalPosition *Position, positions []*Position, nodesPerDepth []int, max_depth int) {
+	if len(nodesPerDepth) == 0 {
+		return
+	}
+	depth := max_depth - len(nodesPerDepth) + 1
+	exp_nodes := nodesPerDepth[0]
+	newPositions := make([]*Position, 0)
+	for _, pos := range positions {
+		for _, move := range pos.ValidMoves() {
+			newPos := pos.copy()
+			newPos.Update(move)
+			newPositions = append(newPositions, newPos)
+		}
+	}
+	got_nodes := len(newPositions)
+	if exp_nodes != got_nodes {
+		t.Log(originalPosition.String())
+		t.Log(originalPosition.board.Draw())
+		t.Errorf("Depth: %d Expected: %d Got: %d", depth, exp_nodes, got_nodes)
+	}
+
+	countMoves(t, originalPosition, newPositions, nodesPerDepth[1:], max_depth)
+}
+
 func BenchmarkValidMoves(b *testing.B) {
 	pos := unsafeFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 	b.ResetTimer()
