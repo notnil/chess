c7fd66226de01fbe8777e2ee756069d1574ebb23
Tilps <Tilps@users.noreply.github.com>
2020-04-08 14:21:43 +1000

    Support decoding long algebratic notation for 960 style castling.

--- notation.go
+++ notation.go
@@ -73,6 +73,20 @@ func (_ LongAlgebraicNotation) Decode(pos *Position, s string) (*Move, error) {
 		} else if (s1 == E1 && s2 == C1) || (s1 == E8 && s2 == C8) {
 			m.addTag(QueenSideCastle)
 		}
+		if (p.Color() == White && pos.Board().bbForPiece(WhiteRook).Occupied(s2)) {
+			if (s2.File() < s1.File()) {
+				m.addTag(QueenSideCastle)
+			} else {
+				m.addTag(KingSideCastle)
+			}
+		}
+		if (p.Color() == Black && pos.Board().bbForPiece(BlackRook).Occupied(s2)) {
+			if (s2.File() < s1.File()) {
+				m.addTag(QueenSideCastle)
+			} else {
+				m.addTag(KingSideCastle)
+			}
+		}
 	} else if p.Type() == Pawn && s2 == pos.enPassantSquare {
 		m.addTag(EnPassant)
 		m.addTag(Capture)
