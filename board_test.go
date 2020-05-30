be4871c6311f39f15481517a33e6c197ee0860e5
Logan Spears <loganjspears@gmail.com>
2020-05-29 19:38:20 -0700

    added support for board rotations, flips, and transpositions

--- board_test.go
+++ board_test.go
@@ -36,3 +36,55 @@ func TestBoardBinarySerialization(t *testing.T) {
 		t.Fatalf("expected board string %s but got %s", s, cpBoard.String())
 	}
 }
+
+func TestBoardRotation(t *testing.T) {
+	fens := []string{
+		"RP4pr/NP4pn/BP4pb/QP4pq/KP4pk/BP4pb/NP4pn/RP4pr",
+		"RNBKQBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbkqbnr",
+		"rp4PR/np4PN/bp4PB/kp4PK/qp4PQ/bp4PB/np4PN/rp4PR",
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+	}
+	g := NewGame()
+	board := g.Position().Board()
+	for i := 0; i < 4; i++ {
+		board = board.Rotate()
+		if fens[i] != board.String() {
+			t.Fatalf("expected board string %s but got %s", fens[i], board.String())
+		}
+	}
+}
+
+func TestBoardFlip(t *testing.T) {
+	g := NewGame()
+	board := g.Position().Board()
+	board = board.Flip(UpDown)
+	b := "RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr"
+	if b != board.String() {
+		t.Fatalf("expected board string %s but got %s", b, board.String())
+	}
+	board = board.Flip(UpDown)
+	b = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+	if b != board.String() {
+		t.Fatalf("expected board string %s but got %s", b, board.String())
+	}
+	board = board.Flip(LeftRight)
+	b = "rnbkqbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBKQBNR"
+	if b != board.String() {
+		t.Fatalf("expected board string %s but got %s", b, board.String())
+	}
+	board = board.Flip(LeftRight)
+	b = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+	if b != board.String() {
+		t.Fatalf("expected board string %s but got %s", b, board.String())
+	}
+}
+
+func TestBoardTranspose(t *testing.T) {
+	g := NewGame()
+	board := g.Position().Board()
+	board = board.Transpose()
+	b := "rp4PR/np4PN/bp4PB/qp4PQ/kp4PK/bp4PB/np4PN/rp4PR"
+	if b != board.String() {
+		t.Fatalf("expected board string %s but got %s", b, board.String())
+	}
+}
