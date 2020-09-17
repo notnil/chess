075f9c7658e7987650faf6dbeaa0334066a2ddda
Aubrey Lavigne <Aubrey.S.Lavigne@gmail.com>
2020-09-17 14:59:05 -0400

    Run TestValidDecoding as one test per scenario

--- notation_test.go
+++ notation_test.go
@@ -29,28 +29,29 @@ func TestValidDecoding(t *testing.T) {
 	}
 
 	for _, test := range validTests {
-		for i, n := range []Notation{AlgebraicNotation{}, LongAlgebraicNotation{}} {
-			moveText := test.AlgText
-			if i == 1 {
-				moveText = test.LongAlgText
-			}
-			m, err := n.Decode(test.Pos1, moveText)
-			if err != nil {
-				movesStrList := []string{}
-				for _, m := range test.Pos1.ValidMoves() {
-					s := n.Encode(test.Pos1, m)
-					movesStrList = append(movesStrList, s)
+		t.Run(test.Description, func(t *testing.T) {
+			for i, n := range []Notation{AlgebraicNotation{}, LongAlgebraicNotation{}} {
+				moveText := test.AlgText
+				if i == 1 {
+					moveText = test.LongAlgText
 				}
-				t.Fatalf("starting from board \n%s\n expected move to be valid error - %s %s\n", test.Pos1.board.Draw(), err, strings.Join(movesStrList, ","))
+				m, err := n.Decode(test.Pos1, moveText)
+				if err != nil {
+					movesStrList := []string{}
+					for _, m := range test.Pos1.ValidMoves() {
+						s := n.Encode(test.Pos1, m)
+						movesStrList = append(movesStrList, s)
+					}
+					t.Fatalf("starting from board \n%s\n expected move to be valid error - %s %s\n", test.Pos1.board.Draw(), err, strings.Join(movesStrList, ","))
+				}
+				postPos := test.Pos1.Update(m)
+				if test.Pos2.String() != postPos.String() {
+					t.Fatalf("starting from board \n%s\n after move %s\n expected board to be %s\n%s\n but was %s\n%s\n",
+						test.Pos1.board.Draw(), m.String(), test.Pos2.String(),
+						test.Pos2.board.Draw(), postPos.String(), postPos.board.Draw())
+				}
 			}
-			postPos := test.Pos1.Update(m)
-			if test.Pos2.String() != postPos.String() {
-				t.Fatalf("starting from board \n%s\n after move %s\n expected board to be %s\n%s\n but was %s\n%s\n",
-					test.Pos1.board.Draw(), m.String(), test.Pos2.String(),
-					test.Pos2.board.Draw(), postPos.String(), postPos.board.Draw())
-			}
-		}
-
+		})
 	}
 }
 
