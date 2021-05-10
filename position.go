1492aafc1b9c26749e17b369c260a5755e0ea11e
Mike Brown <github@torvosoft.com>
2021-05-10 23:16:33 +0000

    Half move clock should not reset on castle
    This change fixes Position.Update() to not reset the half move clock
    on castles. https://handbook.fide.com/chapter/E012018 rule 9.3 only
    mentions captures and pawn moves, not castles. As one example
    reference to another system, importing the following game:
    
        1. d4 Nf6 2. Nf3 d5 3. e3 Bf5 4. c4 c6
        5. Nc3 e6 6. Bd3 Bxd3 7. Qxd3 Nbd7 8. b3 Bd6
        9. O-O
    
    into lichess.org's analyzer outputs the following final position:
    
        r2qk2r/pp1n1ppp/2pbpn2/3p4/2PP4/1PNQPN2/P4PPP/R1B2RK1 b kq - 2 9
    
    and not:
    
        r2qk2r/pp1n1ppp/2pbpn2/3p4/2PP4/1PNQPN2/P4PPP/R1B2RK1 b kq - 0 9

--- position.go
+++ position.go
@@ -75,11 +75,11 @@ func (pos *Position) Update(m *Move) *Position {
 	if pos.turn == Black {
 		moveCount++
 	}
-	cr := pos.CastleRights()
+
 	ncr := pos.updateCastleRights(m)
 	p := pos.board.Piece(m.s1)
 	halfMove := pos.halfMoveClock
-	if p.Type() == Pawn || m.HasTag(Capture) || cr != ncr {
+	if p.Type() == Pawn || m.HasTag(Capture) {
 		halfMove = 0
 	} else {
 		halfMove++
